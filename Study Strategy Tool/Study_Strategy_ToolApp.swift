3084b164a6079fddb941454bf17fd437c7a94fb0
Grace Choi <grace202630@gmail.com>
2024-06-27 07:12:01 -0700

    Initial Commit

---
+++ Study Strategy Tool/Study_Strategy_ToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_Strategy_ToolApp.swift
+//  Study Strategy Tool
+//
+//  Created by Grace Choi on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Study_Strategy_ToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            StudyStrategy()
+        }
+    }
+}
